c843b3328a95451a395469db75bedd46627e0e67
xing.gu <905853860@qq.com>
2026-09-16 02:40:52 +0800

    fix(notification): preserve template JSON strings

--- packages/php/notification-sms/src/Persistence/Model/NotificationTemplateRecord.php
+++ packages/php/notification-sms/src/Persistence/Model/NotificationTemplateRecord.php
@@ -11,5 +11,6 @@ final class NotificationTemplateRecord extends TenantModel
     /** @var string */ protected $name = 'notification_template';
     /** @var array<string, string> */ protected $type = [
         'id' => 'integer', 'tenant_id' => 'integer', 'created_by_member_id' => 'integer', 'revision' => 'integer',
+        'channels_json' => 'string', 'variable_keys_json' => 'string',
     ];
 }
